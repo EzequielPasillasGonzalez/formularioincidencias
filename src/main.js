0a23b7a9188a986bf9fb0fc397dc8a34bf8197da
JohanaJauregui <johana.gomez8825@alumnos.udg.mx>
2024-07-11 20:01:13 -0600

    agregue boostrap

--- src/main.js
+++ src/main.js
@@ -1,4 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import '@/assets/css/bootstrap.min.css'
+/*import '@/assets/js/bootstrap.bundle.min.js'
+import '@/assets/js/bootstrap.bundle.js'
+import '@/assets/js/bootstrap.js'
+import '@/assets/js/bootstrap.min.js'*/
 
 createApp(App).mount('#app')
